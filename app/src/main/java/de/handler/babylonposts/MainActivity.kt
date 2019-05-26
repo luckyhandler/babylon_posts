7c371189cbd93ea3135aa13163edcf18a3b14e9b
Nino Handler <nih@adorsys.de>
2019-05-27 00:09:22 +0200

    Integration into Ui
    - PostAdapter assigned to recyclerView in MainActivity
    - Ensured Internet permission is set & unsecure network traffic is enabled for Android P++
    - Added ViewModel & LiveData  for being able to easily update ListAdapter

--- app/src/main/java/de/handler/babylonposts/MainActivity.kt
+++ app/src/main/java/de/handler/babylonposts/MainActivity.kt
@@ -2,11 +2,25 @@ package de.handler.babylonposts
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import de.handler.core.repository.Repository
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+    private val repository by inject<Repository>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        val adapter = PostAdapter()
+        viewModel.observePosts(repository).observe(this, Observer {
+            adapter.submitList(it)
+        })
+        recyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/de/handler/babylonposts/MainViewModel.kt
@@ -0,0 +1,29 @@
+package de.handler.babylonposts
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import de.handler.core.dto.Post
+import de.handler.core.repository.Repository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class MainViewModel: ViewModel(), CoroutineScope {
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO + job
+
+    private val postLiveData = MutableLiveData<List<Post?>>()
+
+    fun observePosts(repository: Repository): LiveData<List<Post?>> {
+        launch {
+            val posts = repository.fetchPosts()
+            postLiveData.postValue(posts)
+        }
+
+        return postLiveData
+    }
+}

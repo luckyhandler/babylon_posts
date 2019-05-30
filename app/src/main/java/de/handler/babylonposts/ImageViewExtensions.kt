e226a720050787f476f90c552c54775fa1da7960
Nino Handler <nih@adorsys.de>
2019-05-30 10:39:04 +0200

    Working on layout

--- app/src/main/java/de/handler/babylonposts/ImageViewExtensions.kt
+++ app/src/main/java/de/handler/babylonposts/ImageViewExtensions.kt
@@ -1,12 +1,34 @@
 package de.handler.babylonposts
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import timber.log.Timber
 
 fun ImageView.loadUrl(picasso: Picasso, url: String?) {
     Timber.i("ImageView url is $url")
     if (!url.isNullOrBlank()) {
-        picasso.load(url).into(this)
+        picasso
+            .load(url)
+            .into(object : Target {
+                override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+                    // ignore
+                }
+
+                override fun onBitmapFailed(errorDrawable: Drawable?) {
+                    // ignore
+                }
+
+                override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                    val drawable = bitmap?.let { RoundedBitmapDrawableFactory.create(resources, it) }
+                    drawable?.cornerRadius = 16F
+                    drawable?.setAntiAlias(true)
+
+                    setImageDrawable(drawable)
+                }
+            })
     }
 }

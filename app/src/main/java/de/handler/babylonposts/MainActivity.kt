fc32e20306db84ca2d92ba4ca54f834e68e54433
Nino Handler <nih@adorsys.de>
2019-05-31 23:51:18 +0200

    Cleaned up code

--- app/src/main/java/de/handler/babylonposts/MainActivity.kt
+++ app/src/main/java/de/handler/babylonposts/MainActivity.kt
@@ -2,12 +2,8 @@ package de.handler.babylonposts
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import de.handler.core.repository.Repository
-import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
-    private val repository: Repository by inject()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
